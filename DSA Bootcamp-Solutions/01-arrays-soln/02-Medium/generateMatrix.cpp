09bef1c147e2297755c7eeb5a0662b6f15886a0d
mayanktyagi1709 <mayanktyagi1709@gmail.com>
2022-01-06 16:50:18 +0530

    Spiral Matrix II soln

---
+++ DSA Bootcamp-Solutions/01-arrays-soln/02-Medium/generateMatrix.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int floorMod(int x, int y)
+{
+    return ((x % y) + y) % y;
+}
+
+vector<vector<int>> generateMatrix(int n)
+{
+    vector<vector<int>> result(n, vector<int>(n));
+    int cnt = 1;
+    int dir[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    int d = 0;
+    int row = 0;
+    int col = 0;
+    while (cnt <= n * n)
+    {
+        result[row][col] = cnt++;
+        int r = floorMod(row + dir[d][0], n);
+        int c = floorMod(col + dir[d][1], n);
+        // change direction if next cell is non zero
+        if (result[r][c] != 0)
+            d = (d + 1) % 4;
+        row += dir[d][0];
+        col += dir[d][1];
+    }
+    return result;
+}
+
+int main()
+{
+    int n = 3;
+    vector<vector<int>> ans = generateMatrix(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout<<"[";
+        for (int j = 0; j < n; j++)
+        {
+            cout<< ans[i][j] << " ";
+        }
+        cout<<"]";
+    }
+}
