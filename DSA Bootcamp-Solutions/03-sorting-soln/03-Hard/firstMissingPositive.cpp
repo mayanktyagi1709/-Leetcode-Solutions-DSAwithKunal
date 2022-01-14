df7c08beb7116a318579967ccc7a0d179274f5d7
mayanktyagi1709 <mayanktyagi1709@gmail.com>
2022-01-14 20:42:54 +0530

    First Missing Positive soln

---
+++ DSA Bootcamp-Solutions/03-sorting-soln/03-Hard/firstMissingPositive.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> cyclicSort(vector<int> &nums)
+{
+    int i = 0;
+    while (i < nums.size())
+    {
+        if (nums[i] > 0 && nums[i] < nums.size() && (nums[i] != nums[nums[i] - 1]))
+        {
+            // int correct = nums[i]-1;
+            swap(nums[i], nums[nums[i] - 1]);
+        }
+        else
+        {
+            i++;
+        }
+    }
+    return nums;
+}
+int firstMissingPositive(vector<int> &nums)
+{
+    vector<int> res = cyclicSort(nums);
+
+    for (int i = 0; i < res.size(); i++)
+    {
+        if (res[i] != i + 1)
+        {
+            return i + 1;
+        }
+    }
+    return res.size() + 1;
+}
+
+int main()
+{
+    vector<int> nums = {1,2,0}; 
+    cout << firstMissingPositive(nums);
+}
